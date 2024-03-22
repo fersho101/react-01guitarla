202260a69aa3f0960609043cfcb1e001a3ad2858
ferchoo <corral.fdo@gmail.com>
2024-03-22 12:14:04 -0700

    Se agragan props a componente Guitar y se actualiza App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import Header from "./components/Header";
-import Guitar from "./components/Guitar";
-import { useState } from "react";
+import Header from './components/Header'
+import Guitar from './components/Guitar'
+import { useState } from 'react'
+import { db } from './data/db'
 
 function App() {
- 
+	const [data, setData] = useState(db)
 
-  
-  return (
-    <>
-		<Header />
-      
+	return (
+		<>
+			<Header />
+			<main className='container-xl mt-5'>
+				<h2 className='text-center'>Nuestra Colección</h2>
 
-      <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+				<div className='row mt-5'>
+					{data.map(guitar => (
+						<Guitar
+							key={guitar.id}
+							guitar={guitar}
+						/>
+					))}
+				</div>
+			</main>
 
-        <div className="row mt-5">
-          <Guitar />
-
-        </div>
-      </main>
-
-      <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
-          </p>
-        </div>
-      </footer>
-    </>
-  );
+			<footer className='bg-dark mt-5 py-5'>
+				<div className='container-xl'>
+					<p className='text-white text-center fs-4 mt-4 m-md-0'>
+						GuitarLA - Todos los derechos Reservados
+					</p>
+				</div>
+			</footer>
+		</>
+	)
 }
 
-export default App;
+export default App
