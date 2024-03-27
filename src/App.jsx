de7f216f23740672277ba49f83e3e2cf7727bde2
Fernando Corral <118543184+fersh00@users.noreply.github.com>
2024-03-27 11:30:48 -0700

    Se agregan funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,80 @@
 import Header from './components/Header'
 import Guitar from './components/Guitar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { db } from './data/db'
 
 function App() {
+	const initialCart = () => {
+		const localStorageCart = localStorage.getItem('cart')
+		return localStorageCart ? JSON.parse(localStorageCart) : []
+	}
+
 	const [data, setData] = useState(db)
+	const [cart, setCart] = useState(initialCart)
+
+	const MAX_ITEMS = 5
+	const MIN_ITEMS = 1
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
+	function addToCart(item) {
+		const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+		if (itemExists >= 0) {
+			if (cart[itemExists].quantity >= MAX_ITEMS) return
+			const updateCart = [...cart]
+			updateCart[itemExists].quantity++
+			setCart(updateCart)
+		} else {
+			item.quantity = 1
+			setCart([...cart, item])
+		}
+	}
+
+	function removeFromCart(id) {
+		setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+	}
+
+	function increaseQuantity(id) {
+		const updateCart = cart.map(item => {
+			if (item.id === id && item.quantity < MAX_ITEMS) {
+				return {
+					...item,
+					quantity: item.quantity + 1,
+				}
+			}
+			return item
+		})
+		setCart(updateCart)
+	}
+
+	function decreaseQuantity(id) {
+		const updateCart = cart.map(item => {
+			if (item.id === id && item.quantity > MIN_ITEMS) {
+				return {
+					...item,
+					quantity: item.quantity - 1,
+				}
+			}
+			return item
+		})
+		setCart(updateCart)
+	}
+
+	function clearCart() {
+		setCart([])
+	}
 
 	return (
 		<>
-			<Header />
+			<Header
+				cart={cart}
+				removeFromCart={removeFromCart}
+				increaseQuantity={increaseQuantity}
+				decreaseQuantity={decreaseQuantity}
+				clearCart={clearCart}
+			/>
 			<main className='container-xl mt-5'>
 				<h2 className='text-center'>Nuestra Colección</h2>
 
@@ -17,6 +83,9 @@ function App() {
 						<Guitar
 							key={guitar.id}
 							guitar={guitar}
+							cart={cart}
+							setCart={setCart}
+							addToCart={addToCart}
 						/>
 					))}
 				</div>
